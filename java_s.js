2f4ce89e627446a9941258e56c0e7c81354c8be2
full-hd56 <prnn52@gmail.com>
2026-01-31 21:42:25 +0700

    Add files via upload

---
+++ java_s.js
@@ -0,0 +1,15 @@
+
+function nav1_1(){
+    var menuList = document.getElementById("menuList")
+    menuList.style.maxHeight = "0px";
+}
+
+
+function toggleMenu() {
+    if (menuList.style.maxHeight == "0px") {
+        menuList.style.maxHeight = "300px";
+    }
+    else {
+        menuList.style.maxHeight = "0px";
+    }
+}
